8d0a69aff7a6c29459a4cd8dbbde8259f6acb4e7
Duncan Maina <duncandevs@gmail.com>
2017-03-15 04:35:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+class School
+  #attr_accessor
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if !@roster.has_key?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    result = {}
+    @roster.sort.to_h.collect do |key, value|
+      result[key] = value.sort
+    end
+    result
+  end
+end
